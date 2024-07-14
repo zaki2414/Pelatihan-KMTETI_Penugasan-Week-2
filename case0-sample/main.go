8a7f36cb4b4b3c0c5c118b10f0b3bf381c5b7053
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-14 10:47:33 +0700

    create sample case

---
+++ case0-sample/main.go
@@ -0,0 +1,17 @@
+// CASE 0: Menghitung Panjang Sisi Miring Segitiga
+
+package main
+
+import "fmt"
+
+func CountLength(a, b float32) float32 {
+	// write your code below
+}
+
+func main() {
+	a := 2.0
+	b := 3.0
+
+	c := CountLength(float32(a), float32(b))
+	fmt.Println("length of c:", c)
+}
